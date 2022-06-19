573fe9ab2251b2ebfa31f7f8e9221075ba3cebb0
Ananth Abbaraju <36719875+ananth-abbaraju@users.noreply.github.com>
2022-06-19 18:12:49 -0500

    Add files via upload

---
+++ Wordle/NotInList.java
@@ -0,0 +1,18 @@
+import java.io.IOException;import java.util.ArrayList;import java.util.Scanner;import javax.swing.*;import javax.swing.JFrame;import javax.swing.JPanel;import javax.swing.SwingUtilities;import javax.swing.border.Border;import java.util.ArrayList;import java.util.Scanner;import javax.swing.*;import java.awt.Color;import java.awt.event.ActionEvent;import java.awt.event.ActionListener;import java.awt.*;import java.awt.image.*;import java.io.*;import java.util.ArrayList;import javax.imageio.ImageIO;import java.awt.event.KeyListener;import java.awt.event.KeyEvent;
+
+public class NotInList extends JLabel {
+
+    public NotInList() {
+        setSize(215, 60); //281
+        setLocation(265, 110);//270, 90
+        setText("    Not in word list");
+        setFont(new Font("Arial", Font.BOLD, 20));
+        setForeground(Color.WHITE);
+        setBackground(Color.DARK_GRAY);
+        setOpaque(true);
+        setLayout(null);
+        setVisible(true);  
+        setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY,2));
+    }
+    
+}
